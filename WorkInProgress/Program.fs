67be0aa9b2fa7ac01111ef1ca8fbeb623f67486a
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2022-06-12 14:57:50 +0200

    Add WorkInProgress project to not use another solution to do tests

---
+++ WorkInProgress/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let tredigitnumbers = [100 .. 999]
+let numberstotest =
+    tredigitnumbers |> List.allPairs tredigitnumbers |> List.map (fun (x, y) -> x*y) |> List.sortDescending
+let ispalindrome n =
+    let a = string n
+    let b = a.ToCharArray() |> Array.rev |> System.String  // To parse a char[] into a string we need to use the C# one
+    a = b
+let result = List.find ispalindrome numberstotest
+
+printfn $"Result: {result}"
